4bd3501f519fe315ef2e447df632bc9f54c3f4e8
virghiu <virghiu@gmail.com>
2023-07-03 18:44:21 -0400

    feat: add .env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
@@ -8,7 +9,15 @@ app.use("/public",express.static(publicPath));
 app.get("/", function(req, res) {
   const viewPath = __dirname + '/views/index.html';
   res.sendFile(viewPath);
-})
+});
+
+app.get("/json", function(req, res) {
+  const messageStyle = process.env.MESSAGE_STYLE;
+  let message = 'Hello json';
+  message = messageStyle == 'uppercase' ? message.toUpperCase() : message; 
+
+  res.json({"message": message});
+});
 console.log("Hello World");
 
 
