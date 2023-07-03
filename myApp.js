9c90f755f1670d3dc27b6457f89314718faa1480
virghiu <virghiu@gmail.com>
2023-07-03 17:55:55 -0400

    feat: add static files

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 let express = require('express');
 let app = express();
 
-// app.get("/", function(req, res) {
-//   res.send("Hello Express");
-// })
+const publicPath = __dirname + '/public';
+
+app.use("/public",express.static(publicPath));
 
 app.get("/", function(req, res) {
   const viewPath = __dirname + '/views/index.html';
